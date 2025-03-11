1a90e28be0dd53ef826ecf4c20c42fa682d240a3
LilDomJ <83011636+LilDomc@users.noreply.github.com>
2025-03-11 21:55:12 +0100

    Second commit

---
+++ LabWorksY1/p2.py
@@ -0,0 +1,8 @@
+print("Welcome to the second task, please input a name:")
+input1=input()
+print(f"Welcome {input1}")
+print("Please input your cm height to be converted to inches: ")
+input2 = int(input())
+inches = input2 * 0.3937
+print(f"Your heigh of {input2}cm is {inches} in inches")
+print("Why are these codes so simple? Well it didnt specify on what we have to exactly do other than basic python files to commit and publish :>")
